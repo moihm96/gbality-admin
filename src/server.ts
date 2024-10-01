cebccf764ef1b2563f00c5455c3ab7537f615fdb
moihm96 <moihermol@gmail.com>
2024-10-01 16:27:38 +0100

    Implementing cors

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,20 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db";
 import { errorHandler, notFound } from "./middleware/errorMiddleware";
 import userRoutes from "./routes/userRoutes";
+import cors, { CorsOptions } from "cors";
 
 dotenv.config();
 
 connectDB();
 
 const server = express();
+
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+};
+server.use(cors(corsOptions));
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
